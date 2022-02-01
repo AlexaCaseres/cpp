3aee2d9a5d6d609a582970832576ab4e67a2a5dd
Katherine y Caseres <katherinecaceres1753@gmail.com>
2022-01-31 19:15:04 -0600

    agregando ejercicio 2

---
+++ Ejercicios/Ejercicio2/mensajes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string nombre;
+
+    cout << " Ingrese su nombre: ";
+    cin >> nombre;
+
+    cout << " \n Hola mi nombre es: " << nombre << endl;
+    return 0;
+}
